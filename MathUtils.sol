61b44883410c484a2a553abd3291f11f4aa01c45
Elvijs Teikmanis <ete@cunami.lv>
2017-09-29 23:03:30 +0300

    Overflow check on addition util function

--- MathUtils.sol
+++ MathUtils.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.4.15;
 // TODO: Implement safe math
 library MathUtils {
     function add(uint a, uint b) internal constant returns (uint) {
-        return a + b;
+        uint result = a + b;
+
+        require(result >= a);
+
+        return result;
     }
 }
