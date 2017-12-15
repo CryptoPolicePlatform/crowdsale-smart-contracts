1ac711c599f1b26c0bfc46497c73a6b07353fdae
Unknown <e.teiks@gmail.com>
2017-12-15 15:48:28 +0200

    removed todo

--- Utils/Math.sol
+++ Utils/Math.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.4.19;
 
-// TODO: Implement safe math
 library MathUtils {
     function add(uint a, uint b) internal pure returns (uint) {
         uint result = a + b;
@@ -28,7 +27,7 @@ library MathUtils {
         uint result = a * b;
 
         require(result / a == b);
-        
+
         return result;
     }
 }
