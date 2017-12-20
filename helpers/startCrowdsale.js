80c2da5dae9515c18f5f4f9e2cbeb7ffee52ebf9
Unknown <e.teiks@gmail.com>
2017-12-20 18:57:36 +0200

    helper

---
+++ helpers/startCrowdsale.js
@@ -0,0 +1,12 @@
+var CryptoPoliceOfficerToken = artifacts.require("CryptoPoliceOfficerToken");
+var CryptoPoliceCrowdsale = artifacts.require("CryptoPoliceCrowdsale");
+
+module.exports = function () {
+    return CryptoPoliceOfficerToken.deployed().then(function(token) {
+        return CryptoPoliceCrowdsale.deployed().then(function(crowdsale) {
+            return token.transfer(crowdsale.address, "400000000000000000000000000").then(function(result) {
+                return crowdsale.startCrowdsale(token.address);
+            });
+        });
+    });
+}
