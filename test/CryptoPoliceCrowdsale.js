f922e1a92d9c5fd79b3202c2cf1eae0cf52747c5
Unknown <e.teiks@gmail.com>
2017-12-21 15:09:06 +0200

    updated tests

--- test/CryptoPoliceCrowdsale.js
+++ test/CryptoPoliceCrowdsale.js
@@ -8,10 +8,10 @@ const Assert = require('assert');
 // Execution order and state change should be kept in mind
 contract('CryptoPoliceCrowdsale', function(accounts) {
     describe("Before crowdsale started", function () {
-        it("Payments will not get accepted", function () {
+        it("Payments will get rejected", function () {
             return CryptoPoliceCrowdsale.deployed().then(function(crowdsale) {
                 return crowdsale.send(web3.toWei(1, "ether")).then(function() {
-                    Assert.fail("Managed to send ether successfully");
+                    Assert.fail("Payment should have not been accepted");
                 }).catch(function(error) {
                     Assert.ok(error.message.includes('revert'));
                 })
@@ -20,10 +20,10 @@ contract('CryptoPoliceCrowdsale', function(accounts) {
         it("Admin can start crowdsale", startCrowdsale);
     });
     describe("After crowdsale started", function () {
-        it("Payments will not get accepted before exchange rate is set", function () {
+        it("Payments will get rejected before exchange rate is set", function () {
             return CryptoPoliceCrowdsale.deployed().then(function(crowdsale) {
                 return crowdsale.send(web3.toWei(0.01, "ether")).then(function() {
-                    Assert.fail("Managed to send ether successfully");
+                    Assert.fail("Payment should have not been accepted");
                 }).catch(function(error) {
                     Assert.ok(error.message.includes('revert'));
                 })
@@ -36,19 +36,33 @@ contract('CryptoPoliceCrowdsale', function(accounts) {
                 })
             })
         });
-        it("Funds can be transfered", function() {
+        it("Payment under 0.01 ether will get rejected", function() {
             return CryptoPoliceCrowdsale.deployed().then(function(crowdsale) {
-                var value = web3.toWei(0.01, "ether");
-                var balanceBefore = web3.eth.getBalance(accounts[1]);
+                return crowdsale.send(web3.toWei(0.009, "ether")).then(function() {
+                    Assert.fail("Payment should have not been accepted");
+                }).catch(function(error) {
+                    Assert.ok(error.message.includes('revert'));
+                })
+            })
+        });
+        it("Payment of 0.01 ether will yield correct number of tokens in exchange", function() {
+            return CryptoPoliceCrowdsale.deployed().then(function(crowdsale) {
+                const value = web3.toWei(0.01, "ether");
+                const balanceBefore = web3.eth.getBalance(accounts[1]);
                 const gasPrice = 10000000000;
                 return crowdsale.sendTransaction({
                     from: accounts[1],
                     value: value,
                     gasPrice: gasPrice
                 }).then(function(tx) {
-                    var balanceAfter = web3.eth.getBalance(accounts[1]);
-                    var calculatedBalanceAfter = balanceBefore.minus(value).minus(gasPrice * tx.receipt.gasUsed);
-                    Assert.ok(balanceAfter.equals(calculatedBalanceAfter), "Balance mismatch after ether sent");
+                    const balanceAfter = web3.eth.getBalance(accounts[1]);
+                    const calculatedBalanceAfter = balanceBefore.minus(value).minus(gasPrice * tx.receipt.gasUsed);
+                    Assert.equal(balanceAfter.toString(), calculatedBalanceAfter.toString(), "Balance mismatch after ether sent");
+                    return CryptoPoliceOfficerToken.deployed().then(function(token) {
+                        return token.balanceOf.call(accounts[1]).then(function(tokenCount) {
+                            Assert.equal(value.toString(), tokenCount.toString());
+                        })
+                    })
                 }).catch(function(error) {
                     Assert.ok(false, error.message);
                 });
