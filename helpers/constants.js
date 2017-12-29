2d74c391fabb6a94aec6de5741278845b257e9ff
Unknown <e.teiks@gmail.com>
2017-12-29 17:08:25 +0200

    deprecation fix

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,8 +1,8 @@
 const BigNumber = require('bignumber.js');
-GLOBAL.minCap = new BigNumber("12500000e+18");
-GLOBAL.softCap = new BigNumber("40000000e+18");
-GLOBAL.powerCap = new BigNumber("160000000e+18");
-GLOBAL.hardCap = new BigNumber("400000000e+18");
-GLOBAL.minSale = new BigNumber("1e+16");
-GLOBAL.gasPrice = 10000000000;
-GLOBAL.maxUnidentifiedInvestment = minSale.add(1);
+global.minCap = new BigNumber("12500000e+18");
+global.softCap = new BigNumber("40000000e+18");
+global.powerCap = new BigNumber("160000000e+18");
+global.hardCap = new BigNumber("400000000e+18");
+global.minSale = new BigNumber("1e+16");
+global.gasPrice = 10000000000;
+global.maxUnidentifiedInvestment = minSale.add(1);
