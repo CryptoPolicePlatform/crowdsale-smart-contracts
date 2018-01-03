73a5147f0eba5d31a69f6564ea54a6018aec18b8
Unknown <e.teiks@gmail.com>
2018-01-03 13:24:38 +0200

    simplified Burnable

--- contracts/Token/Burnable.sol
+++ contracts/Token/Burnable.sol
@@ -4,13 +4,12 @@ import "./../Utils/Ownable.sol";
 import "./../Utils/Math.sol";
 import "./TotalSupply.sol";
 import "./Balance.sol";
+import "./Crowdsale.sol";
 
-contract Burnable is TotalSupply, Balance, Ownable {
+contract Burnable is TotalSupply, Balance, Ownable, Crowdsale {
     using MathUtils for uint;
 
     event Burn(address account, uint value);
-
-    mapping(address => bool) internal burners;
 
     function burn(uint amount) public grantBurner requiresSufficientBalance(msg.sender, amount) {
         balances[msg.sender] = balances[msg.sender].sub(amount);
@@ -18,16 +17,8 @@ contract Burnable is TotalSupply, Balance, Ownable {
         Burn(msg.sender, amount);
     }
 
-    function grantBurn(address burner) public grantOwner {
-        burners[burner] = true;
-    }
-
-    function removeBurnAccess(address burner) public grantOwner {
-        burners[burner] = false;
-    }
-
     modifier grantBurner {
-        require(burners[msg.sender]);
+        require(isOwner() || isCrowdsale());
         _;
     }
 }
