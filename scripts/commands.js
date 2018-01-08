7793f34b34cee2e619bf6d5816d477131c078fb0
Unknown <e.teiks@gmail.com>
2018-01-08 11:53:04 +0200

    owner commands

--- scripts/commands.js
+++ scripts/commands.js
@@ -11,12 +11,108 @@ module.exports = function(callback) {
         case "start":
             cmd_Start(callback, artifacts);
             break;
+        case "pause":
+            cmd_Pause(callback, artifacts);
+            break;
+        case "unpause":
+            cmd_Unpause(callback, artifacts);
+            break;
         default: callback(`Unknown command: ${args.cmd}`);
     }
 }
 
 function cmd_Start(callback, artifacts) {
     startCrowdsaleHelper(artifacts.token, artifacts.crowdsale).then(() => callback())
+}
+
+function cmd_Pause(callback, artifacts) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.pauseCrowdsale().then(() => callback())
+    })
+}
+
+function cmd_Unpause(callback, artifacts) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.unPauseCrowdsale().then(() => callback())
+    })
+}
+
+function cmd_ProxyExchange(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.proxyExchange(params[0], params[1]).then(() => callback())
+    })
+}
+
+function cmd_EndCrowdsale(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.endCrowdsale(params[0] == "true" || params[0] == "1").then(() => callback())
+    })
+}
+
+function cmd_MarkAddressIdentified(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.markAddressIdentified(params[0]).then(() => callback())
+    })
+}
+
+function cmd_ReturnSuspendedFunds(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.returnSuspendedFunds(params[0]).then(() => callback())
+    })
+}
+
+function cmd_TransferCrowdsaleFunds(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.transferFunds(params[0], params[1]).then(() => callback())
+    })
+}
+
+function cmd_UpdateMaxUnidentifiedInvestment(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.updateMaxUnidentifiedInvestment(params[0]).then(() => callback())
+    })
+}
+
+function cmd_UpdateMinSale(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.updateMinSale(params[0]).then(() => callback())
+    })
+}
+
+function cmd_BurnLeftoverTokens(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.burnLeftoverTokens(params[0]).then(() => callback())
+    })
+}
+
+function cmd_UpdateExchangeRate(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.updateExchangeRate(params[0], params[1], params[2]).then(() => callback())
+    })
+}
+
+function cmd_MoneyBack(callback, artifacts, params) {
+    artifacts.crowdsale.deployed().then(function(crowdsale) {
+        crowdsale.moneyBack(params[0]).then(() => callback())
+    })
+}
+
+function cmd_EnablePublicTransfers(callback, artifacts) {
+    artifacts.token.deployed().then(function(token) {
+        token.enablePublicTransfers().then(() => callback())
+    })
+}
+
+function cmd_AddTokenLock(callback, artifacts, params) {
+    artifacts.token.deployed().then(function(token) {
+        token.addTokenLock(params[0], params[1]).then(() => callback())
+    })
+}
+
+function cmd_ReleaseLockedTokens(callback, artifacts, params) {
+    artifacts.token.deployed().then(function(token) {
+        token.releaseLockedTokens(params[0]).then(() => callback())
+    })
 }
 
 function getArgs() {
