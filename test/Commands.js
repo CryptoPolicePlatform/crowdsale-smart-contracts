472492f02f632e01c3a70f2500fdd03fefae017e
Unknown <e.teiks@gmail.com>
2018-01-09 17:53:44 +0200

    command tests

--- test/Commands.js
+++ test/Commands.js
@@ -1,5 +1,7 @@
 const gas = 800000;
 const Assert = require('assert');
+require('./../helpers/constants');
+const startCrowdsaleHelper = require('./../helpers/startCrowdsale');
 const CryptoPoliceCrowdsale = artifacts.require("CryptoPoliceCrowdsale");
 const CryptoPoliceOfficerToken = artifacts.require("CryptoPoliceOfficerToken");
 const { exec } = require('child_process');
@@ -32,161 +34,182 @@ const runCommand = function (cmdName, params) {
 };
 
 contract("All", function (accounts) {
-    describe("All", function () {
-        it("Start crowdsale", function () {
-            return runCommand("Start").then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return crowdsale.state.call().then(function (state) {
-                        Assert.equal(state.toString(), "1")
-                    })
-                })
-            })
-        });
-        it("Pause crowdsale", function () {
-            return runCommand("Pause").then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return crowdsale.state.call().then(function (state) {
-                        Assert.equal(state.toString(), "3")
-                    })
-                })
-            })
-        });
-        it("Unpause crowdsale", function () {
-            return runCommand("Unpause").then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return crowdsale.state.call().then(function (state) {
-                        Assert.equal(state.toString(), "1")
-                    })
-                })
-            })
-        });
-        it("Lock tokens", function () {
-            return runCommand("AddTokenLock", [1000, 0]).then(function () {
-                return CryptoPoliceOfficerToken.deployed().then(function (token) {
-                    return token.lockedAmount.call().then(function (amount) {
-                        Assert.equal(amount.toString(), "1000");
-                    })
-                })
-            })
-        });
-        it("Mark address identified", function () {
-            return runCommand("MarkAddressIdentified", [accounts[1]]).then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return crowdsale.identifiedAddresses.call(accounts[1]).then(function (result) {
-                        Assert.ok(result)
-                    })
-                })
-            })
-        });
-        it("Update exchange rate", function () {
-            return runCommand("UpdateExchangeRate", [0, 2, 1]).then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return crowdsale.exchangeRates.call(0).then(function (rate) {
-                        Assert.equal(rate[0].toString(), "2");
-                        Assert.equal(rate[1].toString(), "1");
-                    })
-                })
-            })
-        });
-        it("Update max unidentified investment", function () {
-            const newValue = new BigNumber("11e+18");
-            return runCommand("UpdateMaxUnidentifiedInvestment", [newValue]).then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return crowdsale.maxUnidentifiedInvestment.call().then(function (value) {
-                        Assert.equal(value.toString(), newValue.toString());
-                    })
-                })
-            })
-        });
-        it("Update min sale", function () {
-            const newValue = new BigNumber("10e+18");
-            return runCommand("UpdateMinSale", [newValue]).then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return crowdsale.minSale.call().then(function (value) {
-                        Assert.equal(value.toString(), newValue.toString());
-                    })
-                })
-            })
-        });
-        it("Proxy exchange", function () {
-            const amount = new BigNumber("10e+18");
-            return runCommand("ProxyExchange", [accounts[2], amount]).then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return crowdsale.weiSpent.call(accounts[2]).then(function (value) {
-                        Assert.equal(value.toString(), amount.toString());
-                    })
-                })
-            })
-        });
-        it("Return suspended funds", function () {
-            const amount = new BigNumber("12e+18");
+    it("Start crowdsale", function () {
+        return runCommand("Start").then(function () {
             return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                return crowdsale.sendTransaction({
-                    from: accounts[3],
-                    value: amount
-                }).then(function () {
-                    const balanceBefore = web3.eth.getBalance(accounts[3]);
-                    return runCommand("ReturnSuspendedFunds", [accounts[3]]).then(function () {
-                        const expected = balanceBefore.add(amount);
-                        const balanceAfter = web3.eth.getBalance(accounts[3]);
-                        Assert.equal(balanceAfter.toString(), expected);
-                    })
+                return crowdsale.state.call().then(function (state) {
+                    Assert.equal(state.toString(), "1")
                 })
-            });
-        });
-        it("Money back", function () {
-            const amount = new BigNumber("10e+18");
+            })
+        })
+    });
+    it("Pause crowdsale", function () {
+        return runCommand("Pause").then(function () {
             return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                return crowdsale.sendTransaction({
-                    from: accounts[3],
-                    value: amount
-                }).then(function () {
-                    const balanceBefore = web3.eth.getBalance(accounts[3]);
-                    return runCommand("MoneyBack", [accounts[3]]).then(function () {
-                        const balanceAfter = web3.eth.getBalance(accounts[3]);
-                        const expected = balanceBefore.add(amount);
-                        Assert.equal(balanceAfter.toString(), expected);
-                    })
+                return crowdsale.state.call().then(function (state) {
+                    Assert.equal(state.toString(), "3")
                 })
             })
-        });
-        it("End crowdsale", function () {
-            return runCommand("EndCrowdsale", [true]).then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return crowdsale.state.call().then(function (value) {
-                        Assert.equal(value.toString(), "2");
-                    })
+        })
+    });
+    it("Unpause crowdsale", function () {
+        return runCommand("Unpause").then(function () {
+            return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+                return crowdsale.state.call().then(function (state) {
+                    Assert.equal(state.toString(), "1")
                 })
             })
-        });
-        it("Enable public transfers", function () {
-            return runCommand("EnablePublicTransfers").then(function () {
-                return CryptoPoliceOfficerToken.deployed().then(function (token) {
-                    return token.publicTransfersEnabled.call().then(function (value) {
-                        Assert.ok(value);
-                    })
+        })
+    });
+    it("Lock tokens", function () {
+        return runCommand("AddTokenLock", [1000, 0]).then(function () {
+            return CryptoPoliceOfficerToken.deployed().then(function (token) {
+                return token.lockedAmount.call().then(function (amount) {
+                    Assert.equal(amount.toString(), "1000");
                 })
             })
-        });
-        it("Release locked tokens", function () {
-            return runCommand("ReleaseLockedTokens", [0]).then(function () {
-                return CryptoPoliceOfficerToken.deployed().then(function (token) {
-                    return token.lockedAmount.call().then(function (amount) {
-                        Assert.equal(amount, "0")
-                    })
+        })
+    });
+    it("Mark address identified", function () {
+        return runCommand("MarkAddressIdentified", [accounts[1]]).then(function () {
+            return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+                return crowdsale.identifiedAddresses.call(accounts[1]).then(function (result) {
+                    Assert.ok(result)
                 })
             })
-        });
-        it("Burn leftover tokens", function () {
-            return runCommand("BurnLeftoverTokens", [100]).then(function () {
-                return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
-                    return CryptoPoliceOfficerToken.deployed().then(function (token) {
-                        return token.balanceOf.call(crowdsale.address).then(function (amount) {
-                            Assert.equal(amount.toString(), "0")
-                        })
-                    })
+        })
+    });
+    it("Update exchange rate", function () {
+        return runCommand("UpdateExchangeRate", [0, 2, 1]).then(function () {
+            return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+                return crowdsale.exchangeRates.call(0).then(function (rate) {
+                    Assert.equal(rate[0].toString(), "2");
+                    Assert.equal(rate[1].toString(), "1");
+                })
+            })
+        })
+    });
+    it("Update max unidentified investment", function () {
+        const newValue = new BigNumber("11e+18");
+        return runCommand("UpdateMaxUnidentifiedInvestment", [newValue]).then(function () {
+            return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+                return crowdsale.maxUnidentifiedInvestment.call().then(function (value) {
+                    Assert.equal(value.toString(), newValue.toString());
+                })
+            })
+        })
+    });
+    it("Update min sale", function () {
+        const newValue = new BigNumber("10e+18");
+        return runCommand("UpdateMinSale", [newValue]).then(function () {
+            return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+                return crowdsale.minSale.call().then(function (value) {
+                    Assert.equal(value.toString(), newValue.toString());
+                })
+            })
+        })
+    });
+    it("Proxy exchange", function () {
+        const amount = new BigNumber("10e+18");
+        return runCommand("ProxyExchange", [accounts[2], amount]).then(function () {
+            return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+                return crowdsale.weiSpent.call(accounts[2]).then(function (value) {
+                    Assert.equal(value.toString(), amount.toString());
+                })
+            })
+        })
+    });
+    it("Return suspended funds", function () {
+        const amount = new BigNumber("12e+18");
+        return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+            return crowdsale.sendTransaction({
+                from: accounts[3],
+                value: amount
+            }).then(function () {
+                const balanceBefore = web3.eth.getBalance(accounts[3]);
+                return runCommand("ReturnSuspendedFunds", [accounts[3]]).then(function () {
+                    const expected = balanceBefore.add(amount);
+                    const balanceAfter = web3.eth.getBalance(accounts[3]);
+                    Assert.equal(balanceAfter.toString(), expected);
                 })
             })
         });
     });
-})
+    it("Money back", function () {
+        const amount = new BigNumber("10e+18");
+        return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+            return crowdsale.sendTransaction({
+                from: accounts[3],
+                value: amount
+            }).then(function () {
+                const balanceBefore = web3.eth.getBalance(accounts[3]);
+                return runCommand("MoneyBack", [accounts[3]]).then(function () {
+                    const balanceAfter = web3.eth.getBalance(accounts[3]);
+                    const expected = balanceBefore.add(amount);
+                    Assert.equal(balanceAfter.toString(), expected);
+                })
+            })
+        })
+    });
+    it("End crowdsale", function () {
+        return runCommand("EndCrowdsale", [true]).then(function () {
+            return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+                return crowdsale.state.call().then(function (value) {
+                    Assert.equal(value.toString(), "2");
+                })
+            })
+        })
+    });
+    it("Enable public transfers", function () {
+        return runCommand("EnablePublicTransfers").then(function () {
+            return CryptoPoliceOfficerToken.deployed().then(function (token) {
+                return token.publicTransfersEnabled.call().then(function (value) {
+                    Assert.ok(value);
+                })
+            })
+        })
+    });
+    it("Release locked tokens", function () {
+        return runCommand("ReleaseLockedTokens", [0]).then(function () {
+            return CryptoPoliceOfficerToken.deployed().then(function (token) {
+                return token.lockedAmount.call().then(function (amount) {
+                    Assert.equal(amount, "0")
+                })
+            })
+        })
+    });
+    it("Burn leftover tokens", function () {
+        return runCommand("BurnLeftoverTokens", [100]).then(function () {
+            return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+                return CryptoPoliceOfficerToken.deployed().then(function (token) {
+                    return token.balanceOf.call(crowdsale.address).then(function (amount) {
+                        Assert.equal(amount.toString(), "0")
+                    })
+                })
+            })
+        })
+    });
+});
+contract("All", function (accounts) {
+    it("Refund", function () {
+        return startCrowdsaleHelper(CryptoPoliceOfficerToken.deployed(), CryptoPoliceCrowdsale.deployed()).then(function () {
+            return CryptoPoliceCrowdsale.deployed().then(function (crowdsale) {
+                return crowdsale.updateExchangeRate(0, 1, minSale).then(function () {
+                    return crowdsale.sendTransaction({
+                        from: accounts[1],
+                        value: minSale
+                    }).then(function () {
+                        return crowdsale.endCrowdsale(false).then(function() {
+                            const balanceBefore = web3.eth.getBalance(accounts[1]);
+                            return runCommand("Refund", [accounts[1]]).then(function () {
+                                const balanceAfter = web3.eth.getBalance(accounts[1]);
+                                const expected = balanceBefore.add(minSale);
+                                Assert.equal(balanceAfter.toString(), expected.toString());
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+});
