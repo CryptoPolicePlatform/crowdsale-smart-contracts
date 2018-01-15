247844434465c2be28bb1c59ac426fe1c2c62881
Unknown <e.teiks@gmail.com>
2018-01-15 12:41:29 +0200

    updated helper

--- helpers/startCrowdsale.js
+++ helpers/startCrowdsale.js
@@ -1,8 +1,8 @@
-module.exports = function StartCrowdsale(tokenContract, crowdsaleContract) {
+module.exports = function StartCrowdsale(tokenContract, crowdsaleContract, adminAddress) {
     return tokenContract.then(function(token) {
         return crowdsaleContract.then(function(crowdsale) {
             return token.setCrowdsaleContract(crowdsale.address).then(function() {
-                return crowdsale.startCrowdsale(token.address);
+                return crowdsale.startCrowdsale(token.address, adminAddress);
             });
         });
     });
