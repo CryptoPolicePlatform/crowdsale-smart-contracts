7ceabcdfc99e90f9461d3f16c7a781b8e3b72bfc
Elvijs Teikmanis <ete@cunami.lv>
2018-04-20 13:56:28 +0300

    cleanup

--- contracts/Crowdsale/CrowdsaleToken.sol
+++ contracts/Crowdsale/CrowdsaleToken.sol
@@ -4,5 +4,4 @@ interface CrowdsaleToken {
     function transfer(address destination, uint amount) external returns (bool);
     function balanceOf(address account) external constant returns (uint);
     function burn(uint amount) external;
-    function returnTokens(address _address) external returns (uint);
 }
