b7b14d3a4b7a6f247cfda08fe7a6ceaf90cfdeca
Elvijs Teikmanis <ete@cunami.lv>
2018-04-25 16:44:03 +0300

    truffle script to generate token deployment byte code

--- scripts/token-bytecode.js
+++ scripts/token-bytecode.js
@@ -6,11 +6,15 @@ module.exports = function(callback) {
     var data = CryptoPoliceOfficerToken.new.getData("OfficerCoin", "OFCR", {
         data: tokenMeta.bytecode
     });
+    var estimate = web3.eth.estimateGas({data: data});
+
+    console.log("Gas estimate: " + estimate);
+    
     fs.writeFile(process.argv[process.argv.length - 1], data, (error) => {
         if (error) {
             callback(error);
         } else {
             callback();
         }
-    }); 
+    });
 }
