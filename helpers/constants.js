757ec6bd560a4eb51704af8b0674c8e13da1a1bd
Elvijs Teikmanis <ete@cunami.lv>
2018-05-09 11:18:24 +0300

    updated threshold values

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,7 +1,7 @@
 const BigNumber = require('bignumber.js');
-global.minCap = new BigNumber("12500000e+18");
-global.softCap = new BigNumber("51000000e+18");
-global.powerCap = new BigNumber("204000000e+18");
+global.minCap = new BigNumber("270000000e+18");
+global.softCap = new BigNumber("350000000e+18");
+global.powerCap = new BigNumber("490000000e+18");
 global.hardCap = new BigNumber("510000000e+18");
 global.minSale = new BigNumber("1e+16");
 global.gasPrice = 10000000000;
