e5af5630f6099a1619e4ac60a285c1991a4de119
Elvijs Teikmanis <ete@cunami.lv>
2018-05-15 10:54:55 +0300

    cleaned up some things

--- scripts/interactor.js
+++ scripts/interactor.js
@@ -1,16 +1,17 @@
-const truffleContract = require("truffle-contract");
-
 module.exports = function(callback) {
     const payload = JSON.parse(process.argv[process.argv.length - 1]);
     const Contract = artifacts.require(payload.contract.name);
-    const contract = Contract.at(payload.contract.address);
-    const method = contract[payload.exec.method];
 
     Contract.defaults(payload.transactionObject);
-
-    method.apply(null, payload.exec.args).then(result => {
-        console.log(JSON.stringify(result));
-        callback()
+    
+    Contract.deployed().then(contract => {
+        contract[payload.exec.method].apply(null, payload.exec.args)
+        .then(result => {
+            console.log(JSON.stringify(result));
+            callback()
+        }).catch(e => {
+            callback(e.message)
+        })
     }).catch(e => {
         callback(e.message)
     })
