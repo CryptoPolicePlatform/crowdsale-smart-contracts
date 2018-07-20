0e0f28e9f986d5c9f3514425e602eba4b4df41ba
Elvijs Teikmanis <ete@cunami.lv>
2018-07-20 14:17:09 +0300

    admin wrapper bytecode generator

---
+++ scripts/admin-bytecode.js
@@ -0,0 +1,6 @@
+const generator = require("../helpers/gen-deploy-bytecode");
+module.exports = function(callback) {
+    generator(web3, callback, {
+        contractName: "CrowdsaleAdminWrapper"
+    });
+}
